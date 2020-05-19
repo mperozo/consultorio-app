9764f38bb00f1515240531084918e7f89b571cc5
mperozo <perozo.marcos@gmail.com>
2020-05-19 19:58:59 -0300

    Adicionando a URL da api remota no heruko

--- src/app/apiService.js
+++ src/app/apiService.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
+    //baseURL: 'http://localhost:8080'
+    baseURL: 'https://mperozo-consultorio-api.herokuapp.com/' 
 })
 
 
